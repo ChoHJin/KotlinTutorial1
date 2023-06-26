25c923cc0a1c504a4349ea89d9c5b9d6f5f6478d
ChoHJin <cjc9739@naver.com>
2023-06-26 21:52:23 +0900

    feat : 버튼 클릭시 텍스트 뷰의 카운트가 증가하는 기능

--- app/src/main/java/chj/tutorials/kotlintutorialapp1/MainActivity.kt
+++ app/src/main/java/chj/tutorials/kotlintutorialapp1/MainActivity.kt
@@ -2,10 +2,30 @@ package chj.tutorials.kotlintutorialapp1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnClickMe = findViewById<Button>(R.id.mybutton)
+
+        val countingNum = findViewById<TextView>(R.id.btncount)
+
+        var clickedCnt = 0
+
+        btnClickMe.setOnClickListener {
+            clickedCnt++
+
+            countingNum.text = clickedCnt.toString()
+
+            Toast.makeText(this, "Done", Toast.LENGTH_SHORT).show()
+        }
+
+
     }
 }
